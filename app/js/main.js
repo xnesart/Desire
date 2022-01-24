d534483525af8a53cc1343026c552874b7afc146
xnesart <60525875+xnesart@users.noreply.github.com>
2022-01-24 22:14:11 +0300

    v1

---
+++ app/js/main.js
@@ -0,0 +1,40 @@
+$(function () {
+  $(".header__btn").on("click", function () {
+    $(".rightside-menu").removeClass("rightside-menu--close");
+  });
+  $(".rightside-menu__close").on("click", function () {
+    $(".rightside-menu").addClass("rightside-menu--close");
+  });
+
+  $(".header__btn-menu").on("click", function () {
+    $(".menu").toggleClass('menu--open');
+  });
+
+
+  $(".top__slider").slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+  });
+  $(".contact-slider").slick({
+    slidesToShow: 10,
+    slidesToScroll: 10,
+    dots: true,
+    arrows: false,
+  });
+  $(".article-slider__box").slick({
+    dots: false,
+    arrows: true,
+    prevArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/back.svg" alt="left"></button>',
+    nextArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/next.svg" alt="right"></button>',
+  });
+  // Подключаем вкладки галереи
+  var mixer = mixitup(".gallery__inner", {
+    load: {
+      filter: ".living",
+    },
+  });
+});
